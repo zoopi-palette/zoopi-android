8f28d7bf0fd625ea352136399da05b58721513c2
HoJong <pjh00098@gmail.com>
2022-03-28 19:47:22 +0900

    Feat: Add zoopi Color
    Resolves: #6

--- presentation/src/main/java/com/palette/zoopi/ui/theme/Color.kt
+++ presentation/src/main/java/com/palette/zoopi/ui/theme/Color.kt
@@ -2,7 +2,18 @@ package com.palette.zoopi.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val Main1: Color = Color(0xFFea332c)
+val Main2: Color = Color(0xFFEA3A81)
+val Main3: Color = Color(0xFFFF3E3D)
+val Sub: Color = Color(0xFF3B91F5)
+val Error: Color = Color(0xFFFF6A3A)
+val Success: Color = Color(0xFF4EC28A)
+val Grey90: Color = Color(0xFF161D24)
+val Grey80: Color = Color(0xFF28323C)
+val Grey70: Color = Color(0xFF374553)
+val Grey60: Color = Color(0xFF646F7C)
+val Grey50: Color = Color(0xFF8B95A0)
+val Grey40: Color = Color(0xFFC5C8CE)
+val Grey30: Color = Color(0xFFE9EBEE)
+val Grey20: Color = Color(0xFFF7F8F9)
+val Grey10: Color = Color(0xFFFDFDFD)
